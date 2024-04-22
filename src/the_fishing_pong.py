e101345117f8cde29516a2ad06c17a5283a8ac92
natzambrano <nsz220001@utdallas.edu>
2024-04-21 23:18:33 -0500

    ball bouncing function implemented

--- src/the_fishing_pong.py
+++ src/the_fishing_pong.py
@@ -2,10 +2,24 @@ import pygame
 import sys
 
 def init_screen(resolution):
+    # Used to setup the screen
     pygame.init()
     screen = pygame.display.set_mode((resolution[0], resolution[1]))
     pygame.display.set_caption('The Fishing-Pong')
     return screen
+
+def ball_velocity(ball_pos, ball_speed, ball_size, resolution):
+    # Calculates the ball's position and direction
+    ball_pos[0] += ball_speed[0]
+    ball_pos[1] += ball_speed[1]
+
+    if ball_pos[0] <= 0 or ball_pos[0] >= resolution[0] - ball_size:
+        ball_speed[0] = -ball_speed[0]
+
+    if ball_pos[1] <= 0 or ball_pos[1] >= resolution[1] - ball_size:
+        ball_speed[1] = -ball_speed[1]
+    
+    return ball_pos, ball_speed
 
 def main():
     # Screen setup
@@ -15,6 +29,12 @@ def main():
 
     # Colors
     black = (0, 0, 0)
+    white = (255, 255, 255)
+
+    # Initial gameplay values
+    ball_size = 30
+    ball_pos = [resolution[0] // 2, resolution[1] // 2]
+    ball_speed = [5, 5]
 
     while True:
         keys_pressed = pygame.key.get_pressed()
@@ -23,8 +43,12 @@ def main():
                 pygame.quit()
                 sys.exit()
 
-        screen.fill(black)
+        # Calculate ball position
+        ball_pos, ball_speed = ball_velocity(ball_pos, ball_speed, ball_size, resolution)
 
+        # Draw all game elements
+        screen.fill(black)
+        pygame.draw.ellipse(screen, white, pygame.Rect(ball_pos[0], ball_pos[1], ball_size, ball_size))
         pygame.display.flip()
 
         clock.tick(60) 
